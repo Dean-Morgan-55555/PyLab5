f6a261272d8721dab96cd8cd2dc5c7a7e09e38fa
Dean Morgan <dean.morgan@mines.sdsmt.edu>
2024-10-09 17:04:00 -0600

    initial file

---
+++ PyLab05.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+
+  Program: CSC115/170 Lab 05
+ ----------------------------
+  Partner 1:  
+  Partner 2: 
+  Date: 
+      
+      
+  **To get full credit:**
+        * The code must run without errors
+        * You must check for valid swallows and numbers
+        * Your code must implement the four functions listed with correct arguments and return values
+        * The code must calculate the carry weight, number of coconuts and number of swallows correctly
+        * The code will continue running until the user chooses to end (while loop)
+        * You must comment your code well
+        * You must work as a group
+        * You must turn the lab in on time
+
+  
+      
+      
+"""
+# Put any import statements here
+
+
+# ###########################################################
+# get_swallows() function header
+# - arguments: none
+# - returns: swallow type and swallow number
+# ###########################################################
+
+
+
+
+# ###########################################################
+# carry_weight( sw_type, num ) function header
+# - parameters: swallow type and number
+# - returns: carry weight in grams
+# ###########################################################
+
+
+
+
+# ###########################################################
+# num_coconuts( weight, avg_coconut ) function header
+# - parameters: carry load, weight of 1 coconut in grams
+# - returns: fraction of coconuts that can be carried
+# ###########################################################
+
+
+
+
+# ###########################################################
+# num_swallows ( sw_type, avg_coconut ) function header
+# - parameters: type of swallow and weight of 1 coconut in grams
+# - returns: the number of swallows needed
+# ###########################################################
+
+
+
+
+# ###########################################################
+# main code while loop
+# ###########################################################
+
+
+
+
